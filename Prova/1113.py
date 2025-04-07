52d0dd4c22ccc4807d373f7684a1ce613142ae05
Thomas Laureano <thomaslaureano11@gmail.com>
2025-04-07 20:21:53 -0300

    Create 1113.py
    Criado pela Grazie S2

---
+++ Prova/1113.py
@@ -0,0 +1,34 @@
+print("Crescente e Decrescente")
+
+
+# X, Y= list(input())
+# X = int(X)
+# Y = int(Y)
+
+
+while True:
+    xy = list(input().split(" "))
+    break
+
+print(xy)
+for i in xy:
+    print(i)
+    # if xy[0] > xy[1]:
+    #     print("Decrescente")
+    # elif xy[0] < xy[1]:
+    #     print("Crescente")
+    # else:
+    #     break
+
+
+
+while True:
+    xy = list(input().split(" "))
+    x = int(xy[0])
+    y = int(xy[1])
+    if 
+        print("Decrescente")
+    elif x < y:
+        print("Crescente")
+    else:
+        break
