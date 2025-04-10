89ca28c78536a0c25bc49ed81cf09e6de92bef91
Thomas Laureano <thomaslaureano11@gmail.com>
2025-04-10 20:16:15 -0300

    oie

--- 1035 (incompleto).py
+++ 1035 (incompleto).py
@@ -10,13 +10,15 @@ nums = list(input().split(" "))
 
 for i in nums:
     i = int(i)
+    print(type(i))
 
 def teste(a):
     if a[1] > a[2]:
         if a[3] > a[0]:
             if a[2]+a[3] > a[0]+a[1]:
                 if a[2]>0 and a[3]>0:
-                    if a[0]//2 == 0:
+                    if a[0].
+                    //2 == 0:
                         print("Valores aceitos")
                     else:
                         print("Valores nao aceitos")
